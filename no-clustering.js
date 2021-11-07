bb9c2b0869751ba20aceb401ef5201cfc9983560
phucnv282 <phucnv282@gmail.com>
2021-11-07 22:36:37 +0700

    test

--- no-clustering.js
+++ no-clustering.js
@@ -1,26 +1,27 @@
 const express = require("express");
 const port = 3000;
+console.log("test");
 
 const app = express();
 console.log(`Worker number ${process.pid} started`);
 
-app.get('/', (req, res) => {
-    res.send('Hi there! This application does not use clustering...');
-})
+app.get("/", (req, res) => {
+  res.send("Hi there! This application does not use clustering...");
+});
 
-app.get('/api/nocluster', (req, res) => {
-    console.time('noClusterApi');
-    const base = 8;
-    let result = 0;
-    for (let i = Math.pow(base, 7); i >= 0; --i) {
-        result += i + Math.pow(i, 10);
-    }
-    console.timeEnd('noClusterApi');
+app.get("/api/nocluster", (req, res) => {
+  console.time("noClusterApi");
+  const base = 8;
+  let result = 0;
+  for (let i = Math.pow(base, 7); i >= 0; --i) {
+    result += i + Math.pow(i, 10);
+  }
+  console.timeEnd("noClusterApi");
 
-    console.log(`RESULT IS ${result} - ON PROCESS ${process.pid}`);
-    res.send(`Result number is ${result}`);
-})
+  console.log(`RESULT IS ${result} - ON PROCESS ${process.pid}`);
+  res.send(`Result number is ${result}`);
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+  console.log(`App listening on port ${port}`);
+});
